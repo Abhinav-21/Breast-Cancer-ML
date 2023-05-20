3a0edbca684d3965a2444b516343037c2fcece28
Compelling Demigod <abhiitiwarii0@gmail.com>
2023-05-20 15:59:48 +0530

    new models added

---
+++ obesity.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+import warnings
+from sklearn.metrics import confusion_matrix
+
+warnings.filterwarnings("ignore")
+
+data = pd.read_csv("Obesity Classification.csv")
+data = data.dropna()
+
+data['Gender'] = data['Gender'].map({"Male":1, "Female":0})
+
+x = np.array(data[['Age','Gender','Height','Weight','BMI']])
+y = np.array(data[['Label']])
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.10,random_state=42)
+
+model = SVC()
+model.fit(x_train,y_train)
+
+feature = [22, 1, 170, 90, 28]
+print(model.predict(feature))
+
+y_predict = model.predict(x_test)
+print(y_predict)
+
+confusion_matrix = confusion_matrix(y_test,y_predict)
+true_positive = confusion_matrix[0][0]
+false_positive = confusion_matrix[0][1]
+false_negative = confusion_matrix[1][0]
+true_negative = confusion_matrix[1][1]
+
+accuracy = (true_positive + true_negative) / (true_positive + false_positive+false_negative + true_negative)
+precision = true_positive / (true_positive + false_positive)
+recall = true_positive / (true_positive + false_negative)
+f1Score = 2 * (recall * precision) / (recall + precision)
+print(accuracy, precision, recall, f1Score)
